1222aa350a15b53126107bd775b0f53fc81a9194
Zara721 <zaza275@icloud.com>
2024-05-27 14:05:37 -0600

    update progress commit

--- data_filtering.py
+++ data_filtering.py
@@ -1,4 +1,7 @@
 import sqlite3
+
+import numpy as np
+import pandas as pd
 from prettytable import PrettyTable
 
 # Connect to the SQLite database
@@ -9,16 +12,17 @@ cursor = sqliteConnection.cursor()
 
 # Define and execute the query
 query = """
-SELECT me.DocumentIdentifier, me.Counts, me.Themes
-FROM medium_events me
-JOIN (
-    SELECT DISTINCT GCAM
-    FROM medium_events
-    WHERE (Themes LIKE '%KILL%' OR Counts LIKE '%KILL%') AND Counts LIKE '%Canada%' AND (AllNames LIKE '%Indigenous%' OR AllNames LIKE '%First Nation%') AND (AllNames LIKE '%Murder%' OR AllNames LIKE '%Kill%')
-    AND DocumentIdentifier NOT LIKE '%covid%' AND DocumentIdentifier NOT LIKE '%corona%' AND Themes NOT LIKE '%CORONAVIRUS%' AND Themes NOT LIKE '%PANDEMIC%'
-    AND DocumentIdentifier NOT LIKE '%pipeline%' AND Themes NOT LIKE '%PIPELINE%'
-) AS unique_gcam ON me.GCAM = unique_gcam.GCAM;
+SELECT MIN(me.DocumentIdentifier) as DocumentIdentifier, MAX(me.Counts) as Counts, me.Themes
+FROM large_events me
+WHERE (me.Counts LIKE '%KILL%' OR me.AllNames LIKE '%Murder&')
+  AND me.Counts LIKE '%Canada%'
+  AND (me.AllNames LIKE '%First Nation%' OR me.Counts LIKE '%Indigenous%' OR me.Themes LIKE '%INDIGENOUS%')
+  AND me.DocumentIdentifier NOT LIKE '%covid%' AND me.DocumentIdentifier NOT LIKE '%corona%' AND me.Themes NOT LIKE '%PANDEMIC%' AND me.THEMES NOT LIKE '%CORONAVIRUS%'
+  AND me.DocumentIdentifier NOT LIKE '%pipeline%' AND me.Themes NOT LIKE '%PIPELINE%'
+  AND me.Themes NOT LIKE 'EDUCATION%' AND me.Themes NOT LIKE 'GENERAL_GOVERNMENT%' AND me.Themes NOT LIKE 'GENERAL_HEALTH%' AND me.Themes NOT LIKE 'MANMADE_DISASTER_IMPLIED%' AND me.Themes NOT LIKE 'TAX_DISEASE%'
+GROUP BY me.Themes;
 """
+
 cursor.execute(query)
 
 # Fetch all rows from the result set
@@ -39,6 +43,12 @@ for row in results:
 
 # Print the table
 print(table)
+
+# Collect document identifiers (urls) into a list
+document_identifiers = [row[0] for row in results]
+
+# Print the list of document identifiers (urls)
+print(document_identifiers)
 
 # Close the connection to the SQLite database
 sqliteConnection.close()
