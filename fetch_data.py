1448f9b91cd9379fb119f23137ce38827add8795
Zara721 <zaza275@icloud.com>
2024-05-28 12:26:34 -0600

    get and organize data for clustering commit

--- fetch_data.py
+++ fetch_data.py
@@ -7,7 +7,7 @@ sqliteConnection = sqlite3.connect(r'C:\Users\zaza2\canada_us_events.db')
 # Create a cursor object
 cursor = sqliteConnection.cursor()
 
-# Define and execute the query
+# Define SQL query
 query = """
 SELECT MIN(me.DocumentIdentifier) as DocumentIdentifier, me.Themes
 FROM large_events me
@@ -19,7 +19,7 @@ WHERE (me.Counts LIKE '%KILL%' OR me.AllNames LIKE '%Murder&')
   AND me.THEMES NOT LIKE 'EDUCATION%' AND me.THEMES NOT LIKE 'GENERAL_GOVERNMENT%' AND me.THEMES NOT LIKE 'GENERAL_HEALTH%' AND me.THEMES NOT LIKE 'MANMADE_DISASTER_IMPLIED%' AND me.THEMES NOT LIKE 'TAX_DISEASE%'
 GROUP BY me.Themes;
 """
-
+# Executing the query
 cursor.execute(query)
 
 # Fetch all rows from the result set
@@ -28,6 +28,7 @@ results = cursor.fetchall()
 # Convert the results to a DataFrame
 df = pd.DataFrame(results, columns=['article_url', 'themes'])
 
+# Saving the DataFrame to a CSV file named 'url_themes.csv'
 df.to_csv('url_themes.csv', index=False)
 
 # Close the connection to the SQLite database
@@ -40,6 +41,7 @@ def parse_themes(themes):
     return theme_list
 
 
+# Applying the parse_themes function to the 'themes' column of the DataFrame
 df['themes'] = df['themes'].apply(parse_themes)
 
 # create a DataFrame with unique themes as columns
@@ -48,11 +50,23 @@ themes_df = pd.DataFrame(columns=df['themes'].explode().unique())
 #  iterate over each row in the original DataFrame
 for index, row in df.iterrows():
     for theme in row['themes']:
-        # Set the value to 1 if the theme is present, else 0
+        # Set the value to 1 if the theme is present
         themes_df.loc[index, theme] = 1
 
-# fill NaN values with 0 since some articles might not have certain themes
+# fill NaN values with 0 since some articles don't have certain themes
 themes_df.fillna(0, inplace=True)
 
 # save the new DataFrame to a CSV file
 themes_df.to_csv('theme_frequency_table.csv', index=False)
+
+# Reading the previously saved DataFrames
+url_themes_df = pd.read_csv('url_themes.csv')
+theme_frequency_df = pd.read_csv('theme_frequency_table.csv')
+
+# Concatenating the data frames so that the theme_frequency_table has the article_url column
+concatenated_df = pd.concat([theme_frequency_df, url_themes_df['article_url']], axis=1)
+
+# Overwriting the 'theme_frequency_table.csv' file so it has the article_url column
+concatenated_df.to_csv('theme_frequency_table.csv', index=False)
+
+
