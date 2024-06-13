aae59c4bf4a0aaf0101c0c31f576154978080db5
Zara721 <zaza275@icloud.com>
2024-06-13 13:08:42 -0600

    site updates commit

--- visualize_data/get_news_sites.py
+++ visualize_data/get_news_sites.py
@@ -16,15 +16,39 @@ FROM mmiwg_events
 """
 df = pd.read_sql_query(query, sqliteConnection)
 
+
 # Define a function to extract the domain from a URL
 def extract_domain(url):
     return url.split('//')[-1].split('/')[0]
 
+
 # Apply the function to the 'url' column to create a new 'domain' column
 df['domain'] = df['url'].apply(extract_domain)
 
-# Filter domains with counts higher than 15
+domain_variations = {
+    "www.ctvnews.ca": ["winnipeg.ctvnews.ca", "montreal.ctvnews.ca", "calgary.ctvnews.ca", "atlantic.ctvnews.ca",
+                       "barrie.ctvnews.ca", "kitchener.ctvnews.ca", "bc.ctvnews.ca", "windsor.ctvnews.ca",
+                       "ottawa.ctvnews.ca", "vancouverisland.ctvnews.ca"],
+    "thestarphoenix.com": ["www.another-example.com", "www.thestarphoenix.com", "thestarphoenix.com:443"],
+    "theturtleislandnews.com": ["www.theturtleislandnews.com"],
+    "www.huffingtonpost.ca": ["quebec.huffingtonpost.ca", "www.huffingtonpost.com"],
+    "leaderpost.com": ["leaderpost.com:443", "www.thestarphoenix.com", "thestarphoenix.com:443"],
+    "news.nationalpost.com": ["nationalpost.com"],
+    "vancouversun.com": ["www.vancouversun.com", "blogs.vancouversun.com", "vancouversun.com:443"],
+    "montrealgazette.com": ["montrealgazette.com:443", "www.montrealgazette.com"],
+    "theprovince.com": ["www.theprovince.com"],
+    "ottawacitizen.com": ["ottawacitizen.com:443", "www.ottawacitizen.com"],
+    "calgaryherald.com": ["www.calgaryherald.com"],
+    "edmontonsun.com": ["www.edmontonsun.com"],
+    "winnipegsun.com": ["www.winnipegsun.com"],
+}
+
+# Filter domains with counts higher than 10
 domain_counts = df['domain'].value_counts()
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+pd.set_option('max_colwidth', None)
+print(domain_counts)
 filtered_domains = domain_counts[domain_counts > 10]
 
 # Create a bar graph with domain names on the y-axis
